6c345c4630d7aebd9774c5f3ee554632e0d1085c
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-21 18:45:28 +0900

    メールアドレスのformatに関する有効性を検証する形に

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,20 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@hoge.fuga.org first.last@hoge.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[hoge@fuga..piyo user@example,com user_at_hoge.org user.name@example. hoge@fuga_piyo.com hoge@fuga+piyo.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
