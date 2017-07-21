b50cf85d1fe8036a546acc0c374215fecbabc1f5
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-21 20:32:49 +0900

    password_confirmationは当然passwordと同じ文字列になるべきだった、、、、

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: 'Example User', email: 'user@example.com',
-                     password: 'hogefuga', password_confirmation: 'piyopiyo')
+                     password: 'hogefuga', password_confirmation: 'hogefuga')
   end
 
   test "should be valid" do
