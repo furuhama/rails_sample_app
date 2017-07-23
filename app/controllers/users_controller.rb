979a1ce47f3f1107adf33cf718be25d2410e8b23
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-23 18:26:16 +0900

    user/createのためのページを作成??

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user]) # 実装は終わっていない
+    if @user.save
+      # 保存の成功についてここに
+    else
+      render 'new'
+    end
   end
 end
