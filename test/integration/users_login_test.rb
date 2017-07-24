809d562c8062f044ea20be9749bc6e0d5db77cfe
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-25 00:14:09 +0900

    log_out関数をendしてなかったので修正、logoutとlogout後の動作に関するtestを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -14,7 +14,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty? # 一度表示されたflashが正しく消えているか
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password'} }
     assert_redirected_to @user
@@ -22,6 +22,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template "users/show"
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user) # これはどんなhtmlの内容を検証している？
+    assert_select "a[href=?]", user_path(@user) # これはどんなhtmlの内容を検証している？ -> /users/:user_id へのpathのこと？
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
