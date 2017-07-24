8027e891eec6a26d104074b229943df4bc3043ae
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-24 11:34:45 +0900

    helperにlog_in関数を定義(ブラウザを閉じたら消えるsessionにidwo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報ページへリダイレクトする
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  # 渡されたuserでログインを行う
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
