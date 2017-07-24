b8e6bc5230bf02830e2f9bc21771868e27da8f70
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-24 10:03:30 +0900

    login error時にflash messageを追加(まだ正しくない形)、これを検証するためのintegration testを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報ページへリダイレクトする
     else
-      # エラーメッセージを作成
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくないもの
       render 'new'
     end
   end
