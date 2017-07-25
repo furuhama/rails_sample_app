09cf576134fdc6928195eb90798dc6a616b33377
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-25 10:29:55 +0900

    一つのブラウザで複数タブでアプリケーションを開いているユーザーが一つ目のタブでログアウトした際に２つ目のタブでログアウトしようとするとエラーが起きる問題を検証するtestを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,6 +17,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password'} }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template "users/show"
@@ -26,6 +27,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするをシミュレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
