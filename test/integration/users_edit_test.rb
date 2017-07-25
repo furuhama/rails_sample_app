797be2034217c325eae29f80066ab3304eaa60f0
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-25 19:09:05 +0900

    edit成功時のtestを先に書く

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,4 +14,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select 'div.alert', /4/
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'hoge fuga'
+    email = 'piyo@piyo.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
