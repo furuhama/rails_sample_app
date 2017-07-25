bb1858bdaca68168800e032c3ebe0867d906ecf3
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-25 09:45:11 +0900

    sessions helperにremember関数を定義しsessions controllerにrememberを組み込む

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # 現在ログイン中のユーザーを返す(いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
