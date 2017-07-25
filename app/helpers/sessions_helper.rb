a894b930fd144f9928b48278f067a8e30d80dfe4
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-25 20:12:45 +0900

    current_userかどうかで論理値を返すcurrent_user?関数をsessionsのhelper内に追加しtestを書き換え

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 渡されたユーザーがログイン済みであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
   # 現在sessionが存在すればログイン中のユーザーを返し、そうでない場合、もしcookieの記憶トークンに対応するユーザーを返す
   def current_user
     if ( user_id = session[:user_id] )
