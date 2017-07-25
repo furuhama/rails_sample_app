e9e526634ad2207b9ff6d4b660a55fd4c5d8d0ca
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-25 18:53:53 +0900

    edit失敗時のtest作成
    test追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
-
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:furuhama)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "hoge@invalid",
+                                              password: "fuga",
+                                              password_confirmation: "piyo" } }
+    assert_template 'users/edit'
+    assert_select 'div.alert', /4/
+  end
 end
