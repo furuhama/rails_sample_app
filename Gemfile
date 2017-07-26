b3e762bd976f11ad83d5bca329203b31a6b3c34c
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-26 15:21:07 +0900

    testにlogin後のレイアウトチェックを追加、テスト用のアカウントをたくさん作るためにfakerというgemを追加(なお今 回は本番も使うっっぽい)

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt', '3.1.11'
 # bootstrapを動かすためにjquery-railsを入れる
 gem 'jquery-rails'
+gem 'faker'
 
 group :production do
   gem 'pg'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:furuhama)
+  end
+
   test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +12,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+  end
+
+  test "layout links after logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path
+    assert_select "a[href=?]", user_path
+    assert_select "a[href=?]", edit_user_path
+    assert_select "a[href=?]", logout_path
   end
 end
