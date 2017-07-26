fd999e857b29e48362e4fc001fbe41b1f719793f
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-26 14:42:55 +0900

    users controllerにindexアクションを追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
 
   # 引数で与えられたユーザーのGravatar画像を返す
-  def gravatar_for(user, size: 80 ) # キーワード引数便利だ
+  def gravatar_for(user, options = { size: 80 }) # キーワード引数便利だ
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravarar")
   end
