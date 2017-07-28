859bf7adf28607f23b9eaeac6c5601a765f840e7
furuhama <furuhama.yusuke@moneyforward.co.jp>
2017-07-28 17:29:36 +0900

    画像追加に関するtestを作成、fixtureにtest用画像を追加

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -8,6 +8,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type="file"]'
     # 無効な送信
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -15,9 +16,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # 有効な送信
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert assigns(:micropost).picture? # ここ思いつかなかった、assigns変数は直前に作られたインスタンス変数が入っている
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
