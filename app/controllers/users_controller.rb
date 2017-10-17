33027645dd5e36d6f100acef6ce4632175037ecb
furuhama <furuhama@users.noreply.github.com>
2017-10-17 16:40:33 +0900

    refactor to use %i notation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: %i(index edit update destroy following followers)
+  before_action :correct_user, only: %i(edit update)
   before_action :admin_user, only: :destroy
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :following, :followers]
+  before_action :set_user, only: %i(show edit update destroy following followers)
 
   def index
     @users = User.activated.paginate(page: params[:page])
