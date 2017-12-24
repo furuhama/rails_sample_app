2d470f8c81d49a0cb5ce8f4d13dc368441b91631
furuhama <furuhama@users.noreply.github.com>
2017-12-24 12:22:42 +0900

    Fix typo

--- app/models/user_permission.rb
+++ app/models/user_permission.rb
@@ -1,6 +1,6 @@
 class UserPermission < ApplicationRecord
     belongs_to :user
 
-    vaidates :user_id, presence: true
+    validates :user_id, presence: true
     validates :name, presence: true
 end
