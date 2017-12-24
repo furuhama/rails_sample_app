ac53f997916f9a24414a5be6c919891c7c040c17
furuhama <furuhama@users.noreply.github.com>
2017-12-24 14:37:46 +0900

    Add authentication to Admin, Maintainer

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,17 @@
 module Admin
   class BaseController < ApplicationController
+    before_action :authenticate_admin
+
+    private
+
+    def is_admin?
+      current_user.user_permissions.name_list.include?('admin')
+    end
+
+    def authenticate_admin
+      unless is_admin?
+        redirect_to root_path, alert: 'ピピーッ!!あんたadminじゃないやん!!!!!!!!!!!!!!!!!!'
+      end
+    end
   end
 end
