9fdae2ad9007d88ca79c7ab9e44eccef350d0aa0
furuhama <furuhama@users.noreply.github.com>
2017-12-29 17:45:27 +0900

    Add validation for UserPermission

--- app/models/user_permission.rb
+++ app/models/user_permission.rb
@@ -3,6 +3,8 @@ class UserPermission < ApplicationRecord
 
     validates :user_id, presence: true
     validates :name, presence: true
+    # 現在権限は admin maintainer のみ
+    validates :name, inclusion: { in: %w(admin maintainer) }
 
     scope :name_list, -> { pluck(:name) }
 end
