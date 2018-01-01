0f2b617fcba8f4216a086efab39b0fb8bdb1f102
furuhama <furuhama@users.noreply.github.com>
2018-01-01 16:31:41 +0900

    Set schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171224023947) do
 
-  create_table "microposts", force: :cascade do |t|
+  create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20171224023947) do
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
@@ -32,14 +32,14 @@ ActiveRecord::Schema.define(version: 20171224023947) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "user_permissions", force: :cascade do |t|
+  create_table "user_permissions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
@@ -55,4 +55,5 @@ ActiveRecord::Schema.define(version: 20171224023947) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
