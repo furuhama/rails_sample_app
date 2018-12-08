91394138307254b08c716ede217b803c2ae289a1
furuhama <furuhama@users.noreply.github.com>
2018-12-09 00:02:32 +0900

    Lint app/models

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,14 @@ class User < ApplicationRecord
   before_save :downcase_email
   before_create :create_activation_digest
 
-  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   scope :activated, -> { where(activated: true) }
@@ -101,14 +103,14 @@ class User < ApplicationRecord
 
   private
 
-    # メールアドレスをすべて小文字に
-    def downcase_email
-      self.email.downcase!
-    end
+  # メールアドレスをすべて小文字に
+  def downcase_email
+    self.email.downcase!
+  end
 
-    # 有効化トークンとダイジェストを作成および代入
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  # 有効化トークンとダイジェストを作成および代入
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
--- app/models/user_permission.rb
+++ app/models/user_permission.rb
@@ -1,9 +1,9 @@
 class UserPermission < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
 
-    validates :user_id, presence: true
-    # 現在権限は admin maintainer のみ
-    validates :name, inclusion: { in: %w(admin maintainer) }
+  validates :user_id, presence: true
+  # 現在権限は admin maintainer のみ
+  validates :name, inclusion: { in: %w(admin maintainer) }
 
-    scope :name_list, -> { pluck(:name) }
+  scope :name_list, -> { pluck(:name) }
 end
